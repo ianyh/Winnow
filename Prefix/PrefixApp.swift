302ef24a136e6ba67fea6aed512455b51505b508
Ian Ynda-Hummel <ianynda@gmail.com>
2026-04-22 20:52:02 -0400

    Initial Commit

---
+++ Prefix/PrefixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefixApp.swift
+//  Prefix
+//
+//  Created by Ian Ynda-Hummel on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PrefixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrefixTests/PrefixTests.swift
@@ -0,0 +1,19 @@
+//
+//  PrefixTests.swift
+//  PrefixTests
+//
+//  Created by Ian Ynda-Hummel on 4/22/26.
+//
+
+import Testing
+@testable import Prefix
+
+struct PrefixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
